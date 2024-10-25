c60184b1df9e5c8b31bda5c83a4c25f98b059843
jshiell <james.shiell@springer.com>
2024-10-25 11:20:46 +0200

    not able to commit because signed commits and bypass settings

--- run-me.sh
+++ run-me.sh
@@ -5,4 +5,4 @@ set -o pipefail
 set -o nounset
 
 
-echo "now it's not commitable, thx to signed commits"
+echo "now it's not anymore commitable, thx to signed commits and Do not allow bypassing the above settings "

11df5cd594e40a50b2a00a65cf9be4e1371c0cc6
joergi <1439809+joergi@users.noreply.github.com>
2024-10-25 15:43:53 +0200

    Changes text to 1.0.1

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I'm version 1.2");
+        System.out.println("I'm version 1.0.1 - but latest");
     }
 }

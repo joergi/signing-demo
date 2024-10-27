cae0eef84a68f4e1ce23546c949679c86fab4be0
torvalds <torvalds@linux-foundation.org>
2024-10-27 20:15:43 +0100

    Linus is on fire

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I'm version 1.2 but a bad compromised version");
+        System.out.println("even torvalds like this repo");
     }
 }
